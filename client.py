22f479a38ce653f77a6390ee2f243cb51c681e20
Chris Munte <cmunte132@gmail.com>
2018-09-26 18:45:38 -0400

    Changed server to single use
    Server is now single use and needs to be restarted after the client disconnects or calls shutdown

--- client.py
+++ client.py
@@ -4,4 +4,4 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 if __name__ == "__main__":
-    client.bi
+    client.connect(('localhost', 6969))
--- server.py
+++ server.py
@@ -4,14 +4,14 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 def server_loop():
+    conn, addr = server.accept()
+    print('got connection from: ' + addr[0])
     while True:
-        conn, addr = server.accept()
-        print('got connection from: ' + addr[0])
-        while True:
-            got = conn.recv(4096)
-            if got == "QUIT":
-                conn.close()
+        got = conn.recv(4096)
+        if got != "":
             print(got)
+        if got == "SHUTDOWN":
+            conn.close()
 
 
 if __name__ == "__main__":
