f62c52e1dd034db44d252310be6b0be136e79358
Evan Shriner <eshriner@umich.edu>
2018-09-26 18:51:04 -0400

    start client

--- client.py
+++ client.py
@@ -1,7 +1,15 @@
 import socket
 
+HOST = 'localhost'  # The server's hostname or IP address
+PORT = 6969        # The port used by the server
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
 if __name__ == "__main__":
-    client.connect(('localhost', 6969))
+
+    client.connect((HOST, PORT))
+    client.sendall(b'Hello, world')
+    data = client.recv(1024)
+
+print('Received', repr(data))
+
